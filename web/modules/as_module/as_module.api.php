73a586b90c0712da83605a4586b2db42a1780f36
Andrew Stanton <mail@andrewstanton.tech>
2023-04-16 21:34:00 +0100

    Defined and invoked a hook that returns a list of horsemen

---
+++ web/modules/as_module/as_module.api.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @file
+ * This file documents the hook that this module defines.
+ *
+ * This is not run. Just used as an example.
+ */
+
+/**
+ * Returns an array of Horsemen.
+ *
+ * @return string[]
+ *   List of horsemen.
+ */
+function hook_as_module_horsemen_info() {
+  return ['DDoS', 'Merge conflicts'];
+}
