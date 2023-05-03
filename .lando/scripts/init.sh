bf2c901f652b5ea7008b0abb539034a95709cc52
Andrew Stanton <mail@andrewstanton.tech>
2023-05-03 23:14:47 +0100

    Fix: Missed the path updates

--- .lando/scripts/init.sh
+++ .lando/scripts/init.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Copy all the necessary files for drupal.
-if [ ! -f /app/docroot/sites/default/settings.local.php ]; then
-  cp /app/docroot/sites/default/settings.local.php.sample /app/docroot/sites/default/settings.local.php
+if [ ! -f /app/web/sites/default/settings.local.php ]; then
+  cp /app/web/sites/default/settings.local.php.sample /app/web/sites/default/settings.local.php
 fi
 
 # If the site doesn't have a settings.php.
-if [ ! -f /app/docroot/sites/default/settings.php ]; then
-  cp /app/docroot/sites/default/default.settings.php /app/docroot/sites/default/settings.php
+if [ ! -f /app/web/sites/default/settings.php ]; then
+  cp /app/web/sites/default/default.settings.php /app/web/sites/default/settings.php
 fi
 
-if [ ! -d /app/docroot/sites/default/files ]; then
-  mkdir -p /app/docroot/sites/default/files
-  chmod -R 777 /app/docroot/sites/default/files
+if [ ! -d /app/web/sites/default/files ]; then
+  mkdir -p /app/web/sites/default/files
+  chmod -R 777 /app/web/sites/default/files
 fi
 
-if [ ! -d /app/docroot/sites/default/private ]; then
-  mkdir -p /app/docroot/sites/default/private
-  chmod -R 777 /app/docroot/sites/default/private
+if [ ! -d /app/web/sites/default/private ]; then
+  mkdir -p /app/web/sites/default/private
+  chmod -R 777 /app/web/sites/default/private
 fi
 
-chmod 755 /app/docroot/sites/default
+chmod 755 /app/web/sites/default
